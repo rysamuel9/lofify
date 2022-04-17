0bf312caf9ca92e025c7ceff66ea40e66f312b2d
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-04-17 21:28:56 +0700

    sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,8 @@
+import { Box, List, ListIcon, ListItem, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
+import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+
+const Sidebar = () => {
+  return <div>Hello from sidebar</div>;
+};
+
+export default Sidebar;
