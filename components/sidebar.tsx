f88987d905ae049264a9b5106137901aaf221d2f
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-04-17 22:33:10 +0700

    adding logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,17 @@
 import { Box, List, ListIcon, ListItem, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+import NextImage from 'next/image';
 
 const Sidebar = () => {
-  return <div>Hello from sidebar</div>;
+  return (
+    <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/lofify.svg" height={60} width={120} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Sidebar;
