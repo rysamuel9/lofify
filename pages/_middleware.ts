caf020c02e8091fcc65af0adfd485dced2bf132e
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-04-20 20:37:29 +0700

    fix token

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,7 +5,7 @@ const signedinPages = ['/', '/playlist', '/library'];
 
 export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    const token = req.cookies.LOFIFY_ACCESS_TOKEN;
 
     if (!token) {
       return NextResponse.redirect('/signin');
