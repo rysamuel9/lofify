a2c596991e41bd1366aa2f991d0083408431dc38
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-04-20 21:18:50 +0700

    fixed

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,5 @@
 // interact with APPI
-export default function fetcher(url: String, data = undefined) {
+export default function fetcher(url: string, data = undefined) {
   return fetch(`${window.location.origin}/api${url}`, {
     method: data ? 'POST' : 'GET',
     credentials: 'include',
@@ -7,5 +7,10 @@ export default function fetcher(url: String, data = undefined) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.status > 399 && res.status < 200) {
+      throw new Error();
+    }
+    return res.json();
   });
 }
